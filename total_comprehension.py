b13347b8355f20dfc6cd1224e840faa29b4dd8a0
wonseoke <wonseoke@gmail.com>
2020-05-26 15:24:06 -0400

    Mapped List completed

--- total_comprehension.py
+++ total_comprehension.py
@@ -8,3 +8,23 @@ print("--------------")
 print("TOTAL COMPREHENSION...")
 
 # TODO: write python code here
+
+
+mapped_list = []
+for i in my_numbers:
+    mapped_list.append(i * 100)
+
+print("--------------")
+print("Mapped List: " + str(mapped_list))
+
+
+
+'''arr = [1, 2, 3, 4]
+arr2 = []
+
+for i in arr:
+  arr2.append(i * 100)
+
+arr #> [1, 2, 3, 4]
+arr2 #> [100, 200, 300, 400]
+'''
