af6f52e2981596b1ace35edbf0a846643f3f8775
wonseoke <wonseoke@gmail.com>
2020-05-26 15:15:09 -0400

    Basic Set up

---
+++ total_comprehension.py
@@ -0,0 +1,10 @@
+# total_comprehension.py
+
+my_numbers = [1, 2, 3, 4, 5, 6, 7]
+print("--------------")
+print("ORIGINAL LIST:", my_numbers)
+
+print("--------------")
+print("TOTAL COMPREHENSION...")
+
+# TODO: write python code here
